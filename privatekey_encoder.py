4eff62dafbf54ce644495252486b4043ac89f6a5
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-07-19 21:22:23 +0200

    Add encoder function to encode FCM service key

---
+++ privatekey_encoder.py
@@ -0,0 +1,23 @@
+"""
+À utiliser uniquement en mode production ou développement
+
+Ce script permet d'encoder votre clé privée Firebase pour la placer dans un fichier .env
+"""
+
+import json
+from base64 import b64encode as encoder
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Pour le mode production ou développement,
+# Merci de changer la référence pour éviter de surcharger le projet Firebase de l'appli en mode déployé
+REFERENCE = "monpetittrafic-secret-key.json"
+
+with open(REFERENCE,'r') as file:
+    k = json.load(file)
+    k = json.dumps(k)
+encoded = encoder(k.encode('utf-8'))
+
+# Copier le résultat dans le fichier .env
+# en tant que variable ENCODED_FIREBASE_SERVICE_KEY
+print(encoded)

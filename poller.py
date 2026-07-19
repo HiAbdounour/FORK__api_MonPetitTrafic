4df2de8c42424db2eefca82ba4c503f2baf207e2
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-07-19 20:55:00 +0200

    Initialise some constants

---
+++ poller.py
@@ -0,0 +1,13 @@
+from functions import *
+
+# ========= CONSTANTES DE CONTRÔLE ============
+MAX_PARALLEL_FETCHES = 1 #=à changer plus tard
+PER_HOST_CONCURRENCY = 2 #=à voir si on le change
+PER_HOST_INTERVAL = 1.0 # temps minimum avant nouvel essai #=à changer plus tard
+
+#=I DON'T KNOW !!!!!!!!!!!!!!!!!!!!!!!!!
+#BACKOFF_BASE = 2.0
+#MAX_BACKOFF = 600              # seconds (10 min)
+#COOLDOWN_AFTER_ERRORS = 3
+#COOLDOWN_MIN = 60   
+
